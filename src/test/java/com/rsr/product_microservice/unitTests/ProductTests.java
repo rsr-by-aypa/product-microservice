7e8b8b12c1026c4ea3fc6725de03b50eac9ee6e3
Grischakrombacha <grischa.storch@gmail.com>
2024-06-17 12:37:09 +0200

    Added Product Getter/Setter Test Case

--- src/test/java/com/rsr/product_microservice/unitTests/ProductTests.java
+++ src/test/java/com/rsr/product_microservice/unitTests/ProductTests.java
@@ -2,6 +2,7 @@ package com.rsr.product_microservice.unitTests;
 
 
 import com.rsr.product_microservice.core.domain.model.Product;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -12,6 +13,13 @@ public class ProductTests {
     @Test
     void createProductWithNoArgsConstructor() {
         Product product = new Product();
+    }
+
+    @Test
+    void setAndGetProductNameTest() {
+        Product product = new Product();
+        product.setName("Name");
+        Assertions.assertEquals("Name", product.getName());
     }
 
 
