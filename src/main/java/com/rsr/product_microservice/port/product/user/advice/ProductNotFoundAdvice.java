d05c233a294fae1e7a013666ea62702184a08651
Grischakrombacha <grischa.storch@gmail.com>
2024-06-19 14:44:41 +0200

    Added Exception Handler for NoProducts and UnknownProductIdException

---
+++ src/main/java/com/rsr/product_microservice/port/product/user/advice/ProductNotFoundAdvice.java
@@ -0,0 +1,34 @@
+package com.rsr.product_microservice.port.product.user.advice;
+
+import com.rsr.product_microservice.port.product.user.exceptions.NoProductsException;
+import com.rsr.product_microservice.port.product.user.exceptions.UnknownProductIdException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class ProductNotFoundAdvice {
+
+    @ResponseBody
+    @ExceptionHandler(value = UnknownProductIdException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    String unknownProductIdHandler(UnknownProductIdException e){
+        return e.getMessage();
+    }
+
+    @ResponseBody
+    @ExceptionHandler(value = IllegalArgumentException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    String illegalArgumentHandler(IllegalArgumentException e){
+        return e.getMessage();
+    }
+
+    @ResponseBody
+    @ExceptionHandler(value = NoProductsException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    String noProductsHandler(NoProductsException e){
+        return e.getMessage();
+    }
+}
--- src/main/java/com/rsr/product_microservice/port/product/user/exceptions/UnknownProductIdException.java
+++ src/main/java/com/rsr/product_microservice/port/product/user/exceptions/UnknownProductIdException.java
@@ -2,8 +2,8 @@ package com.rsr.product_microservice.port.product.user.exceptions;
 
 import java.util.NoSuchElementException;
 
-public class UnknownIdException extends NoSuchElementException {
-    public UnknownIdException() {
+public class UnknownProductIdException extends NoSuchElementException {
+    public UnknownProductIdException() {
         super("Could not find the passed id. Such Element does not exist.");
     }
 }
