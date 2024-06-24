bd5015800a8c11699b1eed01dd400abd46ca5b39
Grischakrombacha <grischa.storch@gmail.com>
2024-06-24 14:22:30 +0200

    Added ProductUpdateProducer and Method at TestProducerController

--- src/main/java/com/rsr/product_microservice/port/user/controller/TestProducerController.java
+++ src/main/java/com/rsr/product_microservice/port/user/controller/TestProducerController.java
@@ -1,6 +1,7 @@
 package com.rsr.product_microservice.port.user.controller;
 
 import com.rsr.product_microservice.core.domain.model.Product;
+import com.rsr.product_microservice.port.shopping_cart.producer.ProductUpdateProducer;
 import com.rsr.product_microservice.port.user.producer.ProductProducer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,12 +15,14 @@ public class TestProducerController {
     @Autowired
     ProductProducer productProducer;
 
+    @Autowired
+    ProductUpdateProducer productUpdateProducer;
+
     private static final Logger LOGGER = LoggerFactory.getLogger(TestProducerController.class);
 
     @GetMapping("/product-queue")
     public void triggerProductQueue() {
-        Product product = new Product("Stein", "Ein runder, heilender Stein",
-                23.45, 54, "https://link", 32.4, "green", 3.2);
+        Product product = getExampleProduct();
         try {
             productProducer.sendMessage(product);
         } catch (Exception e) {
@@ -27,6 +30,20 @@ public class TestProducerController {
         }
     }
 
+    @GetMapping("product-update-queue")
+    public void triggerUpdateProductQueue() {
+        Product product = getExampleProduct();
+        try {
+            productUpdateProducer.sendMessage(product);
+        } catch (Exception e) {
+            LOGGER.error("Failed to send Product-Updated-Message");
+        }
+    }
+
+    private Product getExampleProduct() {
+        return new Product("Stein", "Ein runder Stein", 23.45, 54,
+                "http://link", 3.45, "green", 4.3);
+    }
 
 
 }
